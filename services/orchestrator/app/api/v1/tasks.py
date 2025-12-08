2aadd08272254cfc4a484f88fd7cf35e30c19ecc
Rishabh Sethi <rishabhsethi238@gmail.com>
2025-12-08 21:18:59 +0530

    refactor: update serialization logic to use ImageFile schema; modify vision client methods to accept ImageFile

--- services/orchestrator/app/api/v1/tasks.py
+++ services/orchestrator/app/api/v1/tasks.py
@@ -1,17 +1,24 @@
-from typing import List
+import mimetypes
 
-import numpy as np
-from common.utils import img_bytes_to_cv2
+from common.schemas.requests import ImageFile
+from common.utils import img_bytes_to_cv2, img_cv2_to_bytes_io
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
+from fastapi.responses import StreamingResponse
+
+from client import vision_client
 
 
-async def serialize(file: UploadFile = File(...)) -> List[np.ndarray]:
+async def serialize(file: UploadFile = File(...)) -> ImageFile:
     if file.content_type.startswith("image/"):
-        return [img_bytes_to_cv2(await file.read())]
+        return ImageFile(
+            file_name=file.filename,
+            file_content=await file.read(),
+            content_type=file.content_type,
+        )
     elif file.content_type.startswith("video/"):
         raise HTTPException(
             status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            detail="Video files are not supported yet.",
+            detail="Video files will be supported in future.",
         )
     else:
         raise HTTPException(
@@ -23,6 +30,32 @@ async def serialize(file: UploadFile = File(...)) -> List[np.ndarray]:
 router = APIRouter()
 
 
-@router.post("/detect")
-async def test(images: np.ndarray = Depends(serialize)):
-    return {"message": "Test endpoint working"}
+@router.post("/classify", summary="Run classification on an image")
+async def classify(file: ImageFile = Depends(serialize)):
+    pass
+
+
+@router.post("/detect", summary="Run object detection on an image")
+async def detect(file: ImageFile = Depends(serialize)):
+    response = await vision_client.detect(file)
+    img = img_bytes_to_cv2(file.file_content)
+
+    ext = mimetypes.guess_extension(file.content_type)
+    img_bytes = img_cv2_to_bytes_io(img, ext=ext)
+
+    return StreamingResponse(img_bytes, media_type=file.content_type)
+
+
+@router.post("/obb", summary="Run oriented bounding box detection on an image")
+async def obb(file: ImageFile = Depends(serialize)):
+    pass
+
+
+@router.post("/pose", summary="Run pose estimation on an image")
+async def pose(file: ImageFile = Depends(serialize)):
+    pass
+
+
+@router.post("/segment", summary="Run segmentation on an image")
+async def segment(file: ImageFile = Depends(serialize)):
+    pass
--- services/orchestrator/app/client/vision.py
+++ services/orchestrator/app/client/vision.py
@@ -2,7 +2,7 @@ from enum import StrEnum
 from typing import Dict
 
 import httpx
-from common.schemas.requests import VisionFile, VisionRequest
+from common.schemas.requests import ImageFile
 from common.schemas.responses import (
     ClassificationResponse,
     DetectionResponse,
@@ -34,32 +34,32 @@ class VisionClient:
         self,
         method: HttpMethod,
         endpoint: str,
-        vision_file: VisionFile,
+        file: ImageFile,
     ) -> Dict:
         response = await self._client.request(
             method=method.value,
             url=endpoint,
-            files=VisionRequest(file=vision_file).model_dump(),
+            files={"file": file},
         )
         response.raise_for_status()
         return response.json()
 
-    async def classify(self, vision_file: VisionFile) -> ClassificationResponse:
-        response = await self.request(Endpoints.CLASSIFY, vision_file)
+    async def classify(self, file: ImageFile) -> ClassificationResponse:
+        response = await self.request(HttpMethod.POST, Endpoints.CLASSIFY, file)
         return ClassificationResponse.model_validate(response)
 
-    async def detect(self, vision_file: VisionFile) -> DetectionResponse:
-        response = await self.request(Endpoints.DETECT, vision_file)
+    async def detect(self, file: ImageFile) -> DetectionResponse:
+        response = await self.request(HttpMethod.POST, Endpoints.DETECT, file)
         return DetectionResponse.model_validate(response)
 
-    async def obb(self, vision_file: VisionFile) -> OBBResponse:
-        response = await self.request(Endpoints.OBB, vision_file)
+    async def obb(self, file: ImageFile) -> OBBResponse:
+        response = await self.request(HttpMethod.POST, Endpoints.OBB, file)
         return OBBResponse.model_validate(response)
 
-    async def pose(self, vision_file: VisionFile) -> PoseResponse:
-        response = await self.request(Endpoints.POSE, vision_file)
+    async def pose(self, file: ImageFile) -> PoseResponse:
+        response = await self.request(HttpMethod.POST, Endpoints.POSE, file)
         return PoseResponse.model_validate(response)
 
-    async def segment(self, vision_file: VisionFile) -> SegmentResponse:
-        response = await self.request(Endpoints.SEGMENT, vision_file)
+    async def segment(self, file: ImageFile) -> SegmentResponse:
+        response = await self.request(HttpMethod.POST, Endpoints.SEGMENT, file)
         return SegmentResponse.model_validate(response)
